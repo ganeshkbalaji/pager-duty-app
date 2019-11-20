01c5ceaa6259fae2fb7c260edd8c8af8994eb6be
Ganesh Balaji <ganeshkbalaji@Ganeshs-MacBook-Pro.local>
2019-11-20 01:18:33 -0800

    working-app

---
+++ api/server/src/config/config.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+
+
+module.exports = {
+    "development": {
+      "username": "ganeshkbalaji",
+      "password": null,
+      "database": "books",
+      "host": "127.0.0.1",
+      "dialect": "postgres",
+      "operatorsAliases": false
+    },
+    "test": {
+      "username": "ganeshkbalaji",
+      "password": null,
+      "database": "book_test",
+      "host": "127.0.0.1",
+      "dialect": "postgres",
+      "operatorsAliases": false
+    },
+    "production": {
+      "username": "root",
+      "password": null,
+      "database": "database_production",
+      "host": "127.0.0.1",
+      "dialect": "mysql",
+      "operatorsAliases": false
+    }
+}
